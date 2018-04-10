9c12feae87bbc23ebb54a325dc44808b120567b6
erlengra <Erlend_grande91@hotmail.com>
2018-04-10 08:51:10 +0200

    Update mqtt-handler.py

--- mqtt-handler.py
+++ mqtt-handler.py
@@ -18,7 +18,8 @@ values for at least 5 min.
 When called the function activates the notify flag and resets the timer. 
 The message is json formatted and sent to the local db.json file. 
 The db.json will be synchronized  with the url address site using json-server application
-The url will be displayed in terminal for debugging reasons 
+The url will be displayed in terminal for debugging reasons.
+Additional documentation: https://github.com/typicode/json-server
 '''
 def postNotification(url, sensorValue, timestamp):
     counter.NOTIFY = True
@@ -28,8 +29,8 @@ def postNotification(url, sensorValue, timestamp):
                    "lastSensorValue": sensorValue}
     print("posting to: " + url)
     req = requests.post(url, data=postMessage)                          # create a post request and send to db.json
-    pastebin_url = req.text                                             # prepare url for print to terminal (debug)
-    print("The pastebin URL is:%s" % pastebin_url)
+    _url_out = req.text                                                 # prepare url for print to terminal (debug)
+    print("The pastebin URL is:%s" % _url_out)
 
 
 '''
@@ -38,6 +39,81 @@ counter value, reset, increment and flags.
 Internal conditions are designed after assignment specifics.
 '''
 def eval_data(msg, counter):
+    print("data is being evaluated")
+    sensorValue = msg["value"]
+    timestamp = msg["timestamp"]
+    print("value: " + str(sensorValue))
+
+    if sensorValue > options["threshold"]:
+        counter.THRESHOLD_EXCEEDED = True
+        print(counter.value)
+        if counter.value > timer and not counter.NOTIFY:                # Condition for post request
+            postNotification(url, sensorValue, timestamp)
+        else:
+            counter.increment()
+    else:
+        if counter.NOTIFY:
+            counter.increment()
+            if counter.value > timer:
+                counter.NOTIFY = False
+                counter.reset()
+        counter.THRESHOLD_EXCEEDED = False
+        print(counter.value)
+
+
+
+'''
+mqtt specific function. See paho documentation https://github.com/eclipse/paho.mqtt.python
+'''
+def on_log(client, userdata, level, buf):
+    print("log:" + buf)
+
+
+
+'''
+mqtt specific function. See paho documentation https://github.com/eclipse/paho.mqtt.python
+'''
+def on_connect(client, userdata, flags, rc):
+    logging.debug("Connected flags" + str(flags) +
+                  "result code " + str(rc) + "client1_id")
+    if rc == 0:
+        print("connected OK")
+        client.connected_flag = True
+    else:
+        print("Bad connection Returned code = " + rc)
+        client.bad_connection_flag = True
+
+
+
+'''
+mqtt specific function. See paho documentation https://github.com/eclipse/paho.mqtt.python
+'''
+def on_disconnect(client, userdata, flags, rc=0):
+    logging.debug("disconnecting reason  " + str(rc))
+    client.connected_flag = False
+    client.disconnect_flag = True
+    client.subscribe_flag = False
+    print("Disconnected result code " + str(rc))
+
+
+
+'''
+mqtt specific function. See paho documentation. https://github.com/eclipse/paho.mqtt.python
+A json load is added to ease key extraction and generalize the message type.
+The function will also run the eval_data function each time a message is received
+from the mqtt broker.
+'''
+def on_message(client, userdata, msg):
+    topic = msg.topic
+    message = str(msg.payload.decode("utf-8", "ignore"))
+    print("message received" + message)
+    jsonM = json.loads(message)                                     # the message is loaded json
+    eval_data(jsonM, counter)                                       # a call to evaluate the message received
+
+
+
+
+
     print("data is being evaluated")
     sensorValue = msg["value"]
     timestamp = msg["timestamp"]
